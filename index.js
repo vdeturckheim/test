5a5fa5a241fd7fcf9b35b7797c538c63bf73b4ea
Vladimir <vdeturckheim@steamulo.com>
2016-05-23 09:22:21 +0200

    kljh

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+var opbeat = require('opbeat').start({
+  appId: 'f634e334f5',
+  organizationId: '524b0487555c4ee29fefbb61cf3d6f1a',
+  secretToken: '517f5411e5ac40a6ea5054a91b3b0b9bd6858883'
+})
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
